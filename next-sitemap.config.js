7907a8ca29ae05a34a375bd9eecdcfc7b7034e91
Kevin Boon <kevin_boon@outlook.com>
2023-10-29 13:51:59 -0400

    Update
    Rebranding and Update with new logo and welcome page

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://docs.sloppydesigns.com/',
+  siteUrl: 'https://dev.reflow-store.com/',
   generateRobotsTxt: true,
   generateIndexSitemap: false,
   outDir: './out',
--- theme.config.tsx
+++ theme.config.tsx
@@ -39,18 +39,46 @@ function useNextSeoProps() {
 const config: DocsThemeConfig = {
   logo: (
     <>
-    <svg xmlns="http://www.w3.org/2000/svg" height="28" fill="none" viewBox="0 0 499.02 68.36">
-      <path d="M47.5,107.22V120.9H27v41H6.49V107.22Z" transform="translate(-6.49 -93.55)" fill="currentColor" />
-      <path d="M54.34,161.91V107.22h47.85V141.4H74.85v6.84h27.34v13.67Zm27.34-34.18V120.9H74.85v6.83Z" transform="translate(-6.49 -93.55)" fill="currentColor" />
-      <path d="M129.54,120.9h27.34v13.67H129.54v27.34H109V93.55h47.85v13.67H129.54Z" transform="translate(-6.49 -93.55)" fill="currentColor" />
-      <path d="M184.22,93.55v68.36H163.71V93.55Z" transform="translate(-6.49 -93.55)" fill="currentColor" />
-      <path d="M238.91,107.22v54.69H191.06V107.22ZM211.57,120.9v27.34h6.83V120.9Z" transform="translate(-6.49 -93.55)" fill="currentColor" />
-      <path d="M307.27,148.24v13.67H286.76v-6.83H273.09v6.83H252.58V148.24h-6.83v-41h20.5V141.4h6.84V114.06h13.67V141.4h6.84V107.22h20.51v41Z" transform="translate(-6.49 -93.55)" fill="currentColor" />
-      <path d="M341.45,148.24v13.67H320.94V148.24Z" transform="translate(-6.49 -93.55)" fill="currentColor" />
-      <path d="M389.3,93.55v6.84h6.84v54.69H389.3v6.83h-41V93.55Zm-20.51,13.67v41h6.84v-41Z" transform="translate(-6.49 -93.55)" fill="currentColor" />
-      <path d="M403,161.91V107.22h47.85V141.4H423.48v6.84h27.34v13.67Zm27.35-34.18V120.9h-6.84v6.83Z" transform="translate(-6.49 -93.55)" fill="currentColor" />
-      <path d="M478.17,107.22v41H485v-41h20.51V141.4h-6.83v13.68h-6.84v6.83H471.33v-6.83H464.5V141.4h-6.84V107.22Z" transform="translate(-6.49 -93.55)" fill="currentColor" />
-    </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" height="28" fill="none" viewBox="0 0 499.02 68.36">
+        <path d="M47.5,107.22V120.9H27v41H6.49V107.22Z" transform="translate(-6.49 -93.55)" fill="currentColor" />
+        <path
+          d="M54.34,161.91V107.22h47.85V141.4H74.85v6.84h27.34v13.67Zm27.34-34.18V120.9H74.85v6.83Z"
+          transform="translate(-6.49 -93.55)"
+          fill="currentColor"
+        />
+        <path
+          d="M129.54,120.9h27.34v13.67H129.54v27.34H109V93.55h47.85v13.67H129.54Z"
+          transform="translate(-6.49 -93.55)"
+          fill="currentColor"
+        />
+        <path d="M184.22,93.55v68.36H163.71V93.55Z" transform="translate(-6.49 -93.55)" fill="currentColor" />
+        <path
+          d="M238.91,107.22v54.69H191.06V107.22ZM211.57,120.9v27.34h6.83V120.9Z"
+          transform="translate(-6.49 -93.55)"
+          fill="currentColor"
+        />
+        <path
+          d="M307.27,148.24v13.67H286.76v-6.83H273.09v6.83H252.58V148.24h-6.83v-41h20.5V141.4h6.84V114.06h13.67V141.4h6.84V107.22h20.51v41Z"
+          transform="translate(-6.49 -93.55)"
+          fill="currentColor"
+        />
+        <path d="M341.45,148.24v13.67H320.94V148.24Z" transform="translate(-6.49 -93.55)" fill="currentColor" />
+        <path
+          d="M389.3,93.55v6.84h6.84v54.69H389.3v6.83h-41V93.55Zm-20.51,13.67v41h6.84v-41Z"
+          transform="translate(-6.49 -93.55)"
+          fill="currentColor"
+        />
+        <path
+          d="M403,161.91V107.22h47.85V141.4H423.48v6.84h27.34v13.67Zm27.35-34.18V120.9h-6.84v6.83Z"
+          transform="translate(-6.49 -93.55)"
+          fill="currentColor"
+        />
+        <path
+          d="M478.17,107.22v41H485v-41h20.51V141.4h-6.83v13.68h-6.84v6.83H471.33v-6.83H464.5V141.4h-6.84V107.22Z"
+          transform="translate(-6.49 -93.55)"
+          fill="currentColor"
+        />
+      </svg>
     </>
   ),
   project: {
@@ -94,7 +122,7 @@ const config: DocsThemeConfig = {
         );
       }
 
-      if (title === 'About') {
+      if (title === 'Welcome') {
         return (
           <>
             <div style={{ display: 'flex' }}>
@@ -131,7 +159,8 @@ const config: DocsThemeConfig = {
     },
   },
   head: useHead,
-  primaryHue: { dark: 151, light: 151 },
+  primaryHue: { dark: 0, light: 0 },
+  primarySaturation: { dark: 0, light: 0 },
 
   useNextSeoProps: useNextSeoProps,
 };
